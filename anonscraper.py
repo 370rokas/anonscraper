ab006e443a2c20f4145852c8e8c025ed1c64dccd
Rokas <rokas@gamingmix.eu>
2019-12-13 15:40:02 +0000

    main program rewritten in order to use requests and bs4 instead of google

--- anonscraper.py
+++ anonscraper.py
@@ -1,17 +1,25 @@
 #!/usr/bin/python3
+	
+import requests
+from bs4 import BeautifulSoup
+import re
 
-#import googlesearch module using for searching
-from googlesearch import search 
- 
-#getting info
 print("What to search?")
 a = input()
 
-print("How many results?")
-b = input()
+query = a.replace(' ', '+')
+link = "https://www.google.com/search?&q=site%3Aanonfile.com+" + query #thanks to Apoc for giving me that syntax
 
-#query to search 
-tosearch = "site:anonfile.com " + a
+response = requests.get(link)
 
-for j in search(tosearch, tld="co.in", num=int(b), stop=int(b), pause=2): 
-	print(j) 
+returnedtext = response.text
+
+#url finder
+
+urls = re.findall('http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+', returnedtext) #thanks to Drizzy for the regex
+
+for x in range(len(urls)):
+	if "anonfile.com" in urls[x]:
+		if urls[x] != "https://anonfile.com":
+			if not "google." in urls[x]:
+				print(urls[x]) 
