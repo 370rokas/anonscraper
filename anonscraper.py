3240cf4e0ab891dead283065f7b6a51273da1d47
Rokas <rokas@gamingmix.eu>
2019-12-14 23:20:14 +0200

    Update anonscraper.py
    thanks to Apoc for shortening the script a bit

--- anonscraper.py
+++ anonscraper.py
@@ -1,7 +1,5 @@
 #!/usr/bin/python3
-	
 import requests
-from bs4 import BeautifulSoup
 import re
 
 print("What to search?")
@@ -11,15 +9,12 @@ query = a.replace(' ', '+')
 link = "https://www.google.com/search?&q=site%3Aanonfile.com+" + query #thanks to Apoc for giving me that syntax
 
 response = requests.get(link)
-
 returnedtext = response.text
 
 #url finder
-
 urls = re.findall('http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+', returnedtext) #thanks to Drizzy for the regex
 
+#output
 for x in range(len(urls)):
-	if "anonfile.com" in urls[x]:
-		if urls[x] != "https://anonfile.com":
-			if not "google." in urls[x]:
-				print(urls[x]) 
+    if("anonfile.com" in urls[x] and urls[x] != "https://anonfile.com" and not "google." in urls[x]):
+        print(urls[x])
